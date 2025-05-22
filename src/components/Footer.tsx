5920380e24fea0dd1deb667e319596d17e54f795
Guillermo Achondo <guillermo.achondo@Guillermo-Achondo-MacBook-Pro.local>
2025-05-22 16:43:37 -0400

    feat(tailwind): update colors and use built in sizes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full h-[172.09px] bg-gray-400 text-white items-center flex justify-center">
+    <div className="w-full h-[172.09px] bg-gray-500 text-white items-center flex justify-center">
       [LOGO]
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="w-full flex bg-gray-100 h-[64px] justify-between items-center px-[20px] sm:px-[128px]">
-      <Link href="/" className="font-[700] text-gray-300 text-[24px]">
+    <div className="w-full flex bg-gray-100 h-16 justify-between items-center px-5 sm:px-32">
+      <Link href="/" className="font-bold text-gray-400 text-2xl">
         GamerShop
       </Link>
       <Link href="/cart">Cart</Link>
