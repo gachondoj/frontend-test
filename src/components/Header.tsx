34142d619f21ec42740245ead17f3aef190f02db
gachondoj <gachondoj@gmail.com>
2025-05-23 13:34:35 -0400

    feat(header): update header to use context

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
+"use client";
+import { useCart } from "@/context/cartContext";
 import Link from "next/link";
 
 const Header = () => {
+  const { cart } = useCart();
+
   return (
     <div className="w-full flex bg-gray-100 h-16 justify-between items-center px-5 sm:px-32">
       <Link href="/" className="font-bold text-gray-400 text-2xl">
         GamerShop
       </Link>
-      <Link href="/cart">Cart</Link>
+      <Link href="/cart">Cart: {cart.length}</Link>
     </div>
   );
 };
