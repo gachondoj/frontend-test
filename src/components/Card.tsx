b7ed47974a52a0ee756773347fdd2c6d4cc28a45
gachondoj <gachondoj@gmail.com>
2025-05-23 13:39:39 -0400

    feat(card): update card to use context

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,36 +1,41 @@
+import { useCart } from "@/context/cartContext";
+import { Game } from "@/utils/endpoint";
+import { isObjectInList } from "@/utils/isObjectInList";
 import Image from "next/image";
 
-interface CardProps {
-  name: string;
-  description: string;
-  price: number;
-  image: string;
-  genre: string;
-  isNew: boolean;
-}
+const Card = (game: Game) => {
+  const { cart, addToCart, removeFromCart } = useCart();
 
-const Card = (props: CardProps) => {
+  const buttonAction = (game: Game) => {
+    if (isObjectInList(game, cart)) {
+      removeFromCart(game);
+    } else {
+      addToCart(game);
+    }
+  };
+
   return (
     <div className="border-[0.5px] border-[#8F8F8F] rounded-2xl p-6 flex flex-col gap-5 h-[436px] w-[380px]">
       <Image
-        src={props.image}
-        alt={props.name}
+        src={game.image}
+        alt={game.name}
         width={300}
         height={100}
         className="rounded-t-2xl min-w-[332px] w-full h-60"
       />
-      <div className="text-gray-300 font-bold text-base">{props.genre}</div>
+      <div className="text-gray-300 font-bold text-base">{game.genre}</div>
       <div className="flex w-full justify-between text-gray-600 font-bold text-lg max-h-6">
         <div className="text-ellipsis overflow-hidden whitespace-nowrap">
-          {props.name}
+          {game.name}
         </div>
-        <div>${props.price}</div>
+        <div>${game.price}</div>
       </div>
       <button
+        onClick={() => buttonAction(game)}
         type="button"
         className="w-full rounded-lg border border-gray-600 h-14 text-gray-600 font-bold"
       >
-        ADD TO CART
+        {isObjectInList(game, cart) ? "REMOVE" : "ADD TO CART"}
       </button>
     </div>
   );
