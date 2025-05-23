4649c3af39d360b9edea0bc7037450eccdccf108
gachondoj <gachondoj@gmail.com>
2025-05-23 12:58:21 -0400

    feat(context): add Cart Context

---
+++ src/context/cartContext.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Game } from "@/utils/endpoint";
+import {
+  createContext,
+  ReactElement,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const CartContext = createContext<{
+  cart: Game[];
+  addToCart: (game: Game) => void;
+  removeFromCart: (game: Game) => void;
+}>({
+  cart: [],
+  addToCart: () => {},
+  removeFromCart: () => {},
+});
+
+export const CartProvider = ({ children }: { children: ReactElement }) => {
+  const [cart, setCart] = useState<Game[]>([]);
+
+  const addToCart = (game: Game) => {
+    setCart([...cart, game]);
+  };
+
+  const removeFromCart = (game: Game) => {
+    setCart(cart.filter((_game) => game.id !== _game.id));
+  };
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
