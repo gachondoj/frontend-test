c0022fc0d2582acf867ba7d04e0573daef44d2c4
gachondoj <gachondoj@gmail.com>
2025-05-27 11:17:54 -0400

    feat(footer): add link to image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="w-full h-[172.09px] bg-gray-500 text-white items-center flex justify-center">
+    <Link
+      href="/"
+      className="w-full h-[172.09px] bg-gray-500 text-white items-center flex justify-center"
+    >
       <Image src="/applyDigitalLogo.png" alt="Logo" width={200} height={100} />
-    </div>
+    </Link>
   );
 };
 
