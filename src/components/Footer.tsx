fb7cc5abe2ce35606b60423bfb193be4c21764fa
gachondoj <gachondoj@gmail.com>
2025-05-27 11:08:38 -0400

    feat(footer): add logo to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
     <div className="w-full h-[172.09px] bg-gray-500 text-white items-center flex justify-center">
-      [LOGO]
+      <Image src="/applyDigitalLogo.png" alt="Logo" width={200} height={100} />
     </div>
   );
 };
