b57dbee9b2381d1463247e17d2ff15ecfd62917f
gachondoj <gachondoj@gmail.com>
2025-05-27 11:31:58 -0400

    feat(config): update env variable name

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
-export const API_URL = process.env.API_URL || "http://localhost:3000/api";
-console.log(process.env.API_URL);
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+console.log(process.env.NEXT_PUBLIC_API_URL);
